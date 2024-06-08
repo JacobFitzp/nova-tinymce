ed1942ead0a36cc67054b2a845e519d18a89d03d
Jacob Fitzpatrick <34043417+JacobFitzp@users.noreply.github.com>
2024-06-08 19:53:59 +0100

    CS-Fixer

--- src/Http/Requests/AttachImageRequest.php
+++ src/Http/Requests/AttachImageRequest.php
@@ -2,8 +2,8 @@
 
 namespace Jacobfitzp\NovaTinymce\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
 
 /**
  * Attach image form request.
